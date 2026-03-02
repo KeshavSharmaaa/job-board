7a3fc2e8309f7cda32b09e0d33f5dcddfb801b78
KeshavSharmaaa <ksharma8941@gmail.com>
2026-03-03 01:57:06 +0530

    dashboard updated

--- client/src/pages/EmployerDashboard.js
+++ client/src/pages/EmployerDashboard.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import API from "../api";
+import API from "../services/api";
 import Layout from "../components/Layout";
 
 function EmployerDashboard() {
--- client/src/pages/JobDetails.js
+++ client/src/pages/JobDetails.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import API from "../api";
+import API from "../services/api";
 import Layout from "../components/Layout";
 
 function JobDetails() {
