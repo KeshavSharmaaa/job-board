5ccc73e0ed59407001ada9a7d2b46f16a52f368f
KeshavSharmaaa <ksharma8941@gmail.com>
2026-03-02 18:56:08 +0530

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -7,13 +7,12 @@ const API = axios.create({
   },
 });
 
+// Attach token automatically
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
