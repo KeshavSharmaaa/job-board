8838c54f2fbe5439a38948c2d841b8ccdb71816d
KeshavSharmaaa <ksharma8941@gmail.com>
2026-03-02 19:07:29 +0530

    Fix production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://job-board-api-e6bb.onrender.com/api",
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
